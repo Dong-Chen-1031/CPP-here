99d247a44c0d02b364ec53bffb83d3ecb1676e32
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2026-03-27 08:20:18 +0800

    feat: add event listener for extension icon click in background script and update manifest structure in wxt.config.ts

--- extensions/entrypoints/background.ts
+++ extensions/entrypoints/background.ts
@@ -1,3 +1,7 @@
 export default defineBackground(() => {
   console.log("Hello background!", { id: browser.runtime.id });
+
+  browser.action.onClicked.addListener(() => {
+    console.log("Extension icon clicked on tab:", { id: browser.runtime.id });
+  });
 });
