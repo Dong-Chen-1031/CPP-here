26c531fc4f8e53b2a27c13be4a11293175fb4593
Dong-Chen-1031 <dcdcdc1031@gmail.com>
2026-05-17 16:58:21 +0800

    updata packages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
+// import starlight from "@astrojs/starlight";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    integrations: [
+        react(),
+        // starlight({
+        //     title: "C++ Here Docs",
+        //     Logo: `./public/favicon.svg`,
+        //     disable404Route: true,
+        // }),
+    ],
     vite: {
         // @ts-ignore I don't know why tailwindcss types are not working. But it works fine.
         plugins: [tailwindcss()],
